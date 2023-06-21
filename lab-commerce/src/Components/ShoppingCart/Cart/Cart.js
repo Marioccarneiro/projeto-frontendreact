7d4b2e53622210fecfa4685e79021bf1111e7a23
Marioccarneiro <mario.ccarneiro@hotmail.com>
2023-06-20 21:11:06 -0300

    tester

---
+++ lab-commerce/src/Components/ShoppingCart/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <>
+      <h1>Cart</h1>
+      <p>0x Nome do Produto:</p>
+      <button>Remover</button>
+      <p typeof="Number">Valor Total:</p>
+    </>
+  );
+}
+
+export default Cart;
---
+++ lab-commerce/src/Components/ShoppingCart/Items/Item.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Item() {
+  return (
+    <h1>Quantidade de produtos:</h1>
+  )
+}
+
+export default Item
